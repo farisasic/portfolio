fce6705e151bf68bbe0558a637f51fa8c519bb7c
Faris <fasic@my.bcit.ca>
2020-05-27 23:48:35 -0700

    added github icon to homepage img background

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,16 @@ import Fade from "react-reveal/Fade";
 import GameProject from "../images/Game Project Assets/game-project-interface-screenshot-1680.jpg";
 import Landing from "../images/Landing Page Assets/landing-page-website-screenshot-1680.jpg";
 import MovieDb from "../images/MovieDB Assets/moviedb-homepage-screenshot-1680.jpg";
+import { GitHub } from "react-feather";
+import { GITHUBLINK } from "../globals/variables";
+
 
 const Home = () => {
   return (
     <main className='main-home' >
       <Fade left >
         <div className='intro' >
-          <h1 > My name is Faris and I 'm a June 2020 graduate from BCIT TWD(Technical Web Designer) Program, based out of Vancouver, BC.</h1>
+          <h1 > My name is Faris and I'm a June 2020 graduate from BCIT TWD(Technical Web Designer) Program, based out of Vancouver, BC.</h1>
         </div>
       </Fade>
 
@@ -24,17 +27,25 @@ const Home = () => {
         </div>
       </Fade>
 
-      <Fade up >
+      <Fade up duration={2000}>
         <div className='project-pic' >
-          <span className='game-project'>
+
+          <div className='game-project'>
             <img src={`${GameProject}`} alt='javascript-game-issd' />
-          </span>
-          <span className='landing-page'>
+            <a href={GITHUBLINK} rel='noopener noreferrer' target='_blank'>
+              <GitHub size={60} />
+            </a>
+          </div>
+
+          <div className='landing-page'>
             <img src={`${Landing}`} alt='school-landing-page' />
-          </span>
-          <span className='moviedb'>
+            <div className='landing-page-show'></div>
+          </div>
+
+          <div className='moviedb'>
             <img src={`${MovieDb}`} alt='moviedb-using-api' />
-          </span>
+            <div className='moviedb-show'></div>
+          </div>
         </div>
       </Fade>
 
--- src/globals/variables.js
+++ src/globals/variables.js
@@ -1,2 +1,3 @@
 export const EMAIL = "mailto:faris@farkoweb.com";
 export const LINKEDIN = "http://www.linkedin.com/in/faris-44b15989";
+export const GITHUBLINK = "https://github.com/farisasic";
